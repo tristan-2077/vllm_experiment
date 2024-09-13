f430b4d1e45945304ae508a2a8a165e2a2cf28ae
tristan <sen.hou2023@gmail.com>
2024-09-13 21:31:25 +0000

    add export sharedgpt dataset script

---
+++ explore_datasets.py
@@ -0,0 +1,16 @@
+import json
+DATASET_PATH="/home/tristan/benchmark_datasets/ShareGPT_V3_unfiltered_cleaned_split.json"
+# load the dataset
+with open(DATASET_PATH, 'r') as f:
+    dataset = json.load(f)
+    print(f"Loaded dataset from {DATASET_PATH}")
+    print(f"Number of prompts: {len(dataset)}")
+    print(f"Sample prompt: {dataset[2]}")
+
+    total_turns = 0
+    # compute how many turns in each prompt and the average number of turns
+    for prompt in dataset:
+        turns = len(prompt["conversations"]) // 2
+        total_turns += turns
+    print(f"Total number of turns: {total_turns}")
+    print(f"Average number of turns: {total_turns / len(dataset)}")
